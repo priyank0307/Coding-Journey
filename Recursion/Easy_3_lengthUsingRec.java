d18010db55dba27e6dcf20c557e99cc5172b4185
Priyank <priyankraj0307@gmail.com>
2026-08-06 17:01:43 +0530

    Rename recursive function for string length calculation

---
+++ Recursion/Easy_3_lengthUsingRec.java
@@ -0,0 +1,13 @@
+    static int len(String str)
+    {
+        if (str.equals(""))
+            return 0;
+        else
+            return len(str.substring(1)) + 1;
+    }
+
+    public static void main(String[] args)
+    {
+        String str = "GeeksforGeeks";
+        System.out.println(len(str));
+    }
