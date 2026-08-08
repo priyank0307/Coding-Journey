f4333e8a9fd754253dfce8e8fd77e869c06ce482
Priyank <priyankraj0307@gmail.com>
2026-08-08 08:24:29 +0530

    Add recursive function to move zeros in array
    Implement recursive function to move zeros in an array.

---
+++ Recursion/Med_6_movingZero.java
@@ -0,0 +1,18 @@
+static void moveZero(int[] arr, int index, int last){
+  if(index>=last){  
+   System.out.println(Arrays.toString(arr));
+   return;
+  }
+
+if(arr[last]==0){
+  moveZero(arr, index, last-1);
+}else if(arr[index]==0){
+  int temp=arr[index];
+  arr[index]=arr[last];
+  arr[last]=temp;
+
+  moveZero(arr, index, last-1);
+}else{
+  moveZero(arr, index+1, last);
+}
+}
