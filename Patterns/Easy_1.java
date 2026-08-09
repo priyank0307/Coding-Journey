bd62a0030b4d1c143889d1431dc95ac909145a9e
Priyank <priyankraj0307@gmail.com>
2026-08-09 11:42:18 +0530

    Add recursive rectangle printing function
    Implement a recursive function to print a rectangle of asterisks.

---
+++ Patterns/Easy_1.java
@@ -0,0 +1,21 @@
+static void rectangle(int n, int index){
+  if(n==0){
+    return;
+  }
+
+for(int i=0; i<index; i++){
+  System.out.print("*"):
+    }
+  System.out.println();
+  rec(n-1, index);
+}
+
+
+//input: 
+(5, 5)
+//output
+*****
+*****
+*****
+*****
+*****  
