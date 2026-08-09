f684f32dfac699ed65c90664a0b2672af3725df7
Priyank <priyankraj0307@gmail.com>
2026-08-09 11:48:59 +0530

    Add rectangle function to print star patterns
    Implement recursive function to print rectangle pattern.

---
+++ Patterns/Easy_2.java
@@ -0,0 +1,24 @@
+static void rectangle(int n){
+  if(n==0){
+    return;
+  }
+
+rec(n-1, index);
+for(int i=0; i<n; i++){
+  System.out.print("*"):
+    }
+  System.out.println();
+}
+
+
+//input: 
+(5)
+//output
+*
+**
+***
+****
+*****  
+
+Concept used here is stack memory allocation. like the calling part is super imp
+  
