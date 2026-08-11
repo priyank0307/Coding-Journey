69e43c55b4df045babf950e64d969d8fadcb5e80
Priyank <priyankraj0307@gmail.com>
2026-08-11 17:29:00 +0530

    Create Easy_1.java

---
+++ Logic_Building/Easy_1.java
@@ -0,0 +1,14 @@
+public static void main(String[] args){
+  int[] arr={1, 3, -1};
+  int max=0;
+
+  for(int i=0; i<arr.length; i++){
+    for(int j=0; j<arr.length; j++){
+      if(Math.abs(arr[i]-arr[j])+Math.abs(i-j)>max){
+        max=Math.abs(arr[i]-arr[j])+Math.abs(i-j);
+      }
+    }
+  }
+
+System.out.println(max);
+}
