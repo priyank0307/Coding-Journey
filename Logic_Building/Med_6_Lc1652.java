8da1b864ddb897635d34db315fcde8836aade1b5
Priyank <priyankraj0307@gmail.com>
2026-08-16 21:58:38 +0530

    Create Med_6_Lc1652.java

---
+++ Logic_Building/Med_6_Lc1652.java
@@ -0,0 +1,40 @@
+//sliding window
+
+class Solution {
+    public int[] decrypt(int[] code, int k) {
+        int n=code.length;
+        int[] ans=new int[n];
+
+        if(k==0){
+            return ans;
+        }
+
+        int start;
+        int end;
+
+        if(k>0){
+            start=1;
+            end=k;
+        }else{
+            start=n+k;
+            end=n-1;
+        }
+
+        int windowSum=0;
+
+        for(int i=start; i<=end; i++){
+            windowSum+=code[i];
+        }
+
+        for(int i=0; i<n; i++){
+            ans[i]=windowSum;
+
+            windowSum-=code[start%n];
+            start++;
+            end++;
+            windowSum+=code[end%n];
+
+        }
+        return ans;
+    }
+}
