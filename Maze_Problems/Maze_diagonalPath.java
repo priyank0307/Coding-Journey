48f2d07eb70561bc8de14eef5cf51cbebeb7a8eb
Priyank <priyankraj0307@gmail.com>
2026-08-26 20:51:35 +0530

    Implement diagonal pathfinding in Maze_diagonalPath
    Fix the return type of the path function to return ArrayList<String> instead of String. Correctly add paths for diagonal, vertical, and horizontal moves.

---
+++ Maze_Problems/Maze_diagonalPath.java
@@ -0,0 +1,26 @@
+static ArrayList<String> path(String p, int r, int c){
+  if(r==1 && c==1){
+    ArrayList<String> list=new ArrayList<>();
+    list.add(p);
+    return p;
+  }
+
+  ArrayList<String> list1=new ArrayList<>();
+
+  if(r>1 && c>1){
+    ArrayList<String> diagonal=path(p+"D", r-1, c-1);
+    list1.addAll(diagonal);
+  }
+    
+  if(c>1){
+    ArrayList<String> left=path(p+"V", r, c-1);
+    list1.addAll(left);
+  }
+
+  if(r>1){
+    ArrayList<String> left=path(p+"H", r-1, c);
+    list1.addAll(right);
+  }
+
+return list1;
+}
