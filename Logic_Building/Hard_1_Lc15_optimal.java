993d527e5fbeb374d664f26343b0bb8704a7c897
Priyank <priyankraj0307@gmail.com>
2026-08-27 19:10:14 +0530

    Update Hard_1_Lc15_optimal.java

--- Logic_Building/Hard_1_Lc15_optimal.java
+++ Logic_Building/Hard_1_Lc15_optimal.java
@@ -2,7 +2,7 @@ class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> outer=new ArrayList<>();
 
-        Arrays.sort(nums);
+        Arrays.sort(nums);  
 
         for(int i=0 ;i<nums.length-2; i++){
 
