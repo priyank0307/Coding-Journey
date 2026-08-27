ab0223bad31a36d1e23acaa258c760cadd0042ae
Priyank <priyankraj0307@gmail.com>
2026-08-27 17:28:16 +0530

    Create Easy_14_Lc1385.java

---
+++ Logic_Building/Easy_14_Lc1385.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int findTheDistanceValue(int[] arr1, int[] arr2, int d) {
+        int noOfElements=0;
+        for(int i=0; i<arr1.length; i++){
+            int cnt=0; 
+            for(int j=0; j<arr2.length; j++){
+                if(Math.abs(arr1[i]-arr2[j])<=d){
+                    cnt++;
+                }
+                }
+                if(cnt==0){
+                    noOfElements++;
+            }
+        }
+        return noOfElements;
+    }
+}
+
+//not optimal
