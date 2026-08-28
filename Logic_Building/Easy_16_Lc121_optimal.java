cb4f23a1fc2f5871cfee500864b5c1bf0a76b636
Priyank <priyankraj0307@gmail.com>
2026-08-28 20:10:34 +0530

    Create Easy_16_Lc121_optimal.java

---
+++ Logic_Building/Easy_16_Lc121_optimal.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int maxProfit=0;
+        int minPrice=Integer.MAX_VALUE;
+
+        for(int i=0; i<prices.length; i++){
+
+            if(minPrice>prices[i]){
+                minPrice=prices[i];
+            }else{
+                maxProfit=Math.max(maxProfit, prices[i]-minPrice);
+            }
+        }
+
+        return maxProfit;
+    }
+}
