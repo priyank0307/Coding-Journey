867669fdbbb009fd7a2e18d61e2ca2e66af38673
Priyank <priyankraj0307@gmail.com>
2026-08-29 21:59:17 +0530

    Create Hard_3_sudoku_Lc37.java

---
+++ Backtracking/Hard_3_sudoku_Lc37.java
@@ -0,0 +1,82 @@
+class Solution {
+    public void solveSudoku(char[][] board) {
+        solve(board);
+    }
+
+    private boolean solve(char[][] board) {
+        int n = board.length;
+        int row = -1;
+        int col = -1;
+        boolean emptyLeft = true;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == '.') {
+                    row = i;
+                    col = j;
+                    emptyLeft = false;
+                    break;
+                }
+            }
+            if (!emptyLeft) {
+                break;
+            }
+        }
+
+        if (emptyLeft) {
+            return true;
+        }
+
+        for (char number = '1'; number <= '9'; number++) {
+            if (isSafe(board, row, col, number)) {
+                board[row][col] = number;
+                if (solve(board)) {
+                    return true;
+                } else {
+                    board[row][col] = '.';
+                }
+            }
+        }
+        return false;
+    }
+
+    private static void display(char[][] board) {
+        for (char[] row : board) {
+            for (char num : row) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    static boolean isSafe(char[][] board, int row, int col, char num) {
+        // Check row
+        for (int i = 0; i < board.length; i++) {
+            if (board[row][i] == num) {
+                return false;
+            }
+        }
+
+        // Check column
+        for (int i = 0; i < board.length; i++) {
+            if (board[i][col] == num) {
+                return false;
+            }
+        }
+
+        // Check 3x3 subgrid
+        int sqrt = (int) (Math.sqrt(board.length));
+        int rowStart = row - row % sqrt;
+        int colStart = col - col % sqrt;
+
+        for (int r = rowStart; r < rowStart + sqrt; r++) {
+            for (int c = colStart; c < colStart + sqrt; c++) {
+                if (board[r][c] == num) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
