6ee0a9067fc8e2887024f1ef97711b9ef77986a7
Priyank <priyankraj0307@gmail.com>
2026-08-30 23:45:49 +0530

    Create Med_19_Lc49.java

---
+++ Logic_Building/Med_19_Lc49.java
@@ -0,0 +1,15 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String, List<String>> map=new HashMap<>();
+
+        for(int i=0; i<strs.length; i++){
+            char[] ch=strs[i].toCharArray();
+            Arrays.sort(ch);
+            String word=new String(ch);
+            map.putIfAbsent(word, new ArrayList<>());
+            map.get(word).add(strs[i]);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+}
