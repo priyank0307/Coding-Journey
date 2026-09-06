7538fecccabff4179b00add9d9b341cf295e39c2
Priyank <priyankraj0307@gmail.com>
2026-09-06 18:12:35 +0530

    Update BinarySearch.java

--- Searching/BinarySearch.java
+++ Searching/BinarySearch.java
@@ -2,7 +2,7 @@ class Solution {
     public int search(int[] nums, int target) {
 
         int left = 0;
-        int right = nums.length - 1;
+        int right = nums.length - 1;    
 
         while (left <= right) {
 
