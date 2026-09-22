69a576493a35826e1f0314b4da4cca57d13d14a7
Priyank <priyankraj0307@gmail.com>
2026-09-22 20:54:01 +0530

    Update Med_7_Lc79.java

--- Backtracking/Med_7_Lc79.java
+++ Backtracking/Med_7_Lc79.java
@@ -1,4 +1,4 @@
-class Solution {
+class Solution {   
     public boolean exist(char[][] board, String word) {
         for(int i=0; i<board.length; i++){
             for(int j=0; j<board[0].length; j++){
@@ -34,7 +34,7 @@ class Solution {
                     || validation(board, word, row, col-1, index+1)
                     || validation(board, word, row, col+1, index+1);
 
-        //unmarking
+        //unmarking or backtrack
         board[row][col]=temp;            
     return found;
     }
