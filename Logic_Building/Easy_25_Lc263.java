c1ed33c6fe0371d6743d68d1fc3a15f1ff24461c
Priyank <priyankraj0307@gmail.com>
2026-09-26 22:06:20 +0530

    Create Easy_25_Lc263.java

---
+++ Logic_Building/Easy_25_Lc263.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isUgly(int n) {
+        if(n<=0){
+            return false;
+        }
+
+        int[] factors={2, 3, 5};
+
+        for(int factor: factors){
+            while(n%factor==0){
+                n/=factor;
+            }
+        }
+
+        if(n==1){
+            return true;
+        }
+
+        return false;
+    }
+}
